0c467c9d11389309b2bba6135532d1d98527046c
BabyYod4 <razzlenator@protonmail.com>
2020-03-21 13:29:27 +0000

    Added test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# experiments
+# Endnode Transmitter
 
-Generic experiment repo for pushing and updating code that does not still work 100% 
+In this test we use a single rfm95w transmitter to simulate 8 transmitter in series by hopping channels after each sequental transmittion. 
+
+This can than be picked up by a gateway as 8 indidual devices sending at different intervals. For example this test can be used with the `gateway-receiver` test. 
---
+++ src/main.cpp
@@ -0,0 +1,264 @@
+#include "libsoilar/drivers/rfm95w/rfm95w.h"            
+#include INIT_ENVOIRMENT
+
+// Uno Transmitter
+/*
+  DIO0 - D2
+  RESET - D8
+  NSS - D9
+  SCK - D13
+  MOSI - D11
+  MISO - D12
+*/
+
+// Due Transmitter
+/*
+  DIO0 - D22
+  RESET - D12
+  NSS - D13
+  SCK - D74
+  MOSI - D75
+  MISO - D74
+*/
+
+// LoraEndnode transmitter;
+
+// LoraEndnodeSettings settings1 = { 
+//   .frequencyMHz = RFM95W_CHANNEL_868_1,  
+//   .signalBandwidthHz = RFM95W_BW_125KHZ,
+//   .preambleLength = RFM95W_DEFAULT_PREAMBLE_LENGTH,
+//   .txPowerdBm = RFM95W_DEFAULT_TX_POWER,
+//   .spreadingFactor = RFM95W_DEFAULT_SF,
+//   .codeRate = RFM95W_CODE_RATE_4_6,
+//   .syncWord = 0x12,
+//   .enableCRC = true,
+//   .lowPowerReceiveMode = false,
+//   .invertIQ = false
+// };
+
+// LoraEndnodeSettings settings2 = { 
+//   .frequencyMHz = RFM95W_CHANNEL_868_2,  
+//   .signalBandwidthHz = RFM95W_BW_125KHZ,
+//   .preambleLength = RFM95W_DEFAULT_PREAMBLE_LENGTH,
+//   .txPowerdBm = RFM95W_DEFAULT_TX_POWER,
+//   .spreadingFactor = RFM95W_DEFAULT_SF,
+//   .codeRate = RFM95W_CODE_RATE_4_6,
+//   .syncWord = 0x12,
+//   .enableCRC = true,
+//   .lowPowerReceiveMode = false,
+//   .invertIQ = false
+// };
+
+// LoraEndnodeSettings settings3 = { 
+//   .frequencyMHz = RFM95W_CHANNEL_868_3,  
+//   .signalBandwidthHz = RFM95W_BW_125KHZ,
+//   .preambleLength = RFM95W_DEFAULT_PREAMBLE_LENGTH,
+//   .txPowerdBm = RFM95W_DEFAULT_TX_POWER,
+//   .spreadingFactor = RFM95W_DEFAULT_SF,
+//   .codeRate = RFM95W_CODE_RATE_4_6,
+//   .syncWord = 0x12,
+//   .enableCRC = true,
+//   .lowPowerReceiveMode = false,
+//   .invertIQ = false
+// };
+
+// LoraEndnodeSettings settings4 = { 
+//   .frequencyMHz = RFM95W_CHANNEL_868_4,  
+//   .signalBandwidthHz = RFM95W_BW_125KHZ,
+//   .preambleLength = RFM95W_DEFAULT_PREAMBLE_LENGTH,
+//   .txPowerdBm = RFM95W_DEFAULT_TX_POWER,
+//   .spreadingFactor = RFM95W_DEFAULT_SF,
+//   .codeRate = RFM95W_CODE_RATE_4_6,
+//   .syncWord = 0x12,
+//   .enableCRC = true,
+//   .lowPowerReceiveMode = false,
+//   .invertIQ = false
+// };
+
+
+// Rfm95wInterface interface = { 0, 9, 8, 2 };
+
+// uint8_t buff1[8] = {0xFF, 0x01, 0x00, 0x00, 0x0D, 0xDD, 0xFF, 0xFF};
+// uint8_t buff2[8] = {0xFF, 0x02, 0x00, 0x00, 0x4C, 0x54, 0xFF, 0xFF};
+// uint8_t buff3[8] = {0xFF, 0x03, 0x00, 0x00, 0x21, 0xBB, 0xFF, 0xFF};
+// uint8_t buff4[8] = {0xFF, 0x04, 0x00, 0x00, 0x34, 0xA3, 0xFF, 0xFF};
+
+
+// uint8_t i = 0;
+// void setup(){
+
+//   Serial.begin(9600);
+//   Serial.println("LoRa Transmitter");
+
+
+//   if ( CreateRfm95W(&transmitter, &interface, &settings1) == LORA_ERROR_WRONG_DEVICE ) {
+//     Serial.println("Starting LoRa failed!");
+//     while(1);
+//   }
+
+// }
+
+// void loop(){
+
+//   if(i > 250){ i=0; }
+  
+//   buff1[3]=i; buff2[3]=i; buff3[3]=i; buff4[3]=i;
+//   i++;
+
+//   transmitter.send(buff1, 8);
+//   Serial.print("Device 1 - Sending packet '");
+//   Serial.print(buff1[3]); Serial.print("-"); Serial.print(buff1[4]); Serial.print("-"); Serial.print(buff1[5]);
+//   Serial.print("'\r\n");
+//   delay(1000);
+  
+//   transmitter.hop(&settings2);
+//   transmitter.send(buff2, 8);
+//   Serial.print("Device 2 - Sending packet '");
+//   Serial.print(buff2[3]); Serial.print("-"); Serial.print(buff2[4]); Serial.print("-"); Serial.print(buff2[5]);
+//   Serial.print("'\r\n");
+//   delay(950);
+
+//   transmitter.hop(&settings3);
+//   transmitter.send(buff3, 8);
+//   Serial.print("Device 3 - Sending packet '");
+//   Serial.print(buff3[3]); Serial.print("-"); Serial.print(buff3[4]); Serial.print("-"); Serial.print(buff3[5]);
+//   Serial.print("'\r\n");
+//   delay(850);
+  
+//   transmitter.hop(&settings4);
+//   transmitter.send(buff4, 8);
+//   Serial.print("Device 4 - Sending packet '");
+//   Serial.print(buff4[3]); Serial.print("-"); Serial.print(buff4[4]); Serial.print("-"); Serial.print(buff4[5]);
+//   Serial.print("'\r\n");
+  
+//   delay(40000);
+//   transmitter.hop(&settings1);
+
+//   Serial.print("\r\n");
+
+// }
+
+LoraEndnode transceiver;
+
+LoraEndnodeSettings settings = { 
+  .frequencyMHz = RFM95W_CALC_FEQ_FAST( ((long)(867E6) - 125000) ),  
+  .signalBandwidthHz = RFM95W_BW_250KHZ,
+  .preambleLength = RFM95W_DEFAULT_PREAMBLE_LENGTH,
+  .txPowerdBm = RFM95W_DEFAULT_TX_POWER,
+  .spreadingFactor = 10,
+  .codeRate = RFM95W_CODE_RATE_4_6,
+  .syncWord = 0x12,
+  .enableCRC = true,
+  .lowPowerReceiveMode = true,
+  .invertIQ = false
+};
+
+LoraEndnodeSettings settings2 = { 
+  .frequencyMHz = RFM95W_CHANNEL_868_1,  
+  .signalBandwidthHz = RFM95W_BW_125KHZ,
+  .preambleLength = RFM95W_DEFAULT_PREAMBLE_LENGTH,
+  .txPowerdBm = RFM95W_DEFAULT_TX_POWER,
+  .spreadingFactor = RFM95W_DEFAULT_SF,
+  .codeRate = RFM95W_CODE_RATE_4_6,
+  .syncWord = 0x12,
+  .enableCRC = true,
+  .lowPowerReceiveMode = false,
+  .invertIQ = false
+};
+
+Rfm95wInterface interface = { 0, 9, 8, 2 };
+
+uint8_t buff[11] = {0};
+uint8_t buff2[8] = {0xFF, 0x01, 0x00, 0x00, 0x0D, 0xDD, 0xFF, 0xFF};
+
+
+uint8_t change = 0;
+uint8_t i=0;
+bool txMode = true;
+
+// ISR_CODE void received(void);
+
+// void received(void){
+
+//   // received a packet
+//   print("Received packet '");
+//   if ( transceiver.read(buff, 11) == LORA_PKG_CORRUPTED ){
+//     Serial.println("Corrupted");
+//   } else{
+//     Serial.print("Received packet '");
+//     Serial.print(buff[4]); 
+//     Serial.print("-");
+//     Serial.print(buff[5]); 
+//     Serial.print("-"); 
+//     Serial.print(buff[6]); 
+//     Serial.print("-"); 
+//     Serial.print(buff[7]); 
+//     Serial.print("-");
+//     Serial.print(buff[8]); 
+//     Serial.print("'\r\n");   
+//     change++;
+//   }
+  
+// }
+
+void setup(){
+
+  Serial.begin(9600);
+  Serial.println("LoRa Receiver");
+
+
+  if ( CreateRfm95W(&transceiver, &interface, &settings2) == LORA_ERROR_WRONG_DEVICE ) {
+    Serial.println("Starting LoRa failed!");
+    while(1);
+  }
+
+  // transceiver.setOnReceiveCallback( received );
+  transceiver.setMode(LORA_TX_MODE);
+
+}
+
+void loop(){
+
+  // if( receiver.packetReceived() ){
+
+  //   if ( receiver.read(buff, 11) == LORA_PKG_CORRUPTED ){
+  //     Serial.println("Corrupted");
+  //   } else{
+  //     Serial.print("Received packet '");
+  //     Serial.print(buff[4]); 
+  //     Serial.print("-");
+  //     Serial.print(buff[5]); 
+  //     Serial.print("-"); 
+  //     Serial.print(buff[6]); 
+  //     Serial.print("-"); 
+  //     Serial.print(buff[7]);
+  //     Serial.print("-"); 
+  //     Serial.print(buff[8]);
+  //     Serial.print("'\r\n");   
+  //   }
+
+  // }
+  // delay(100);
+  
+  if (txMode){
+    if(i > 250){ i=0; }
+    buff2[3]=i;
+    transceiver.send(buff2, 8);
+    Serial.print("Device [1] -- Sending packet [");
+    Serial.print(buff2[3]); 
+    Serial.print("] "); 
+    Serial.print(buff2[4]); Serial.print(" "); Serial.print(buff2[5]);
+    Serial.print("\r\n");
+    i++;
+    delay(5000);
+  }
+
+  // if (change >= 10){
+  //   transceiver.setMode( LORA_TX_MODE );
+  //   transceiver.hop( &settings2 );
+  //   txMode = true;
+  //   change = 0;
+  //   delay(20000);
+  // }
+  
+}
